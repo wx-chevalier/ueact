152f9b6d768203472f409620e46e0361446a3423
王下邀月熊 <wxchevalier925+wxyyxc1992@gmail.com>
2019-06-16 11:34:05 +0800

    Create storage.ts

---
+++ framework/packages/ueact-utils/src/dom/storage.ts
@@ -0,0 +1,16 @@
+/** 解析 Cookie 字符串 */
+export function parseCookieStr(cookieStr: string) {
+  cookieStr
+    .split(';')
+    .reduce((cookieObject: Record<string, any>, cookieString) => {
+      const splitCookie = cookieString
+        .split('=')
+        .map(cookiePart => cookiePart.trim());
+      try {
+        cookieObject[splitCookie[0]] = JSON.parse(splitCookie[1]);
+      } catch (error) {
+        cookieObject[splitCookie[0]] = splitCookie[1];
+      }
+      return cookieObject;
+    }, {});
+}
