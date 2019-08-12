8921d0541ea07aa5d5320bf2001762699544ca31
wx-chevalier <384924552@qq.com>
2019-08-12 12:52:44 +0800

    feat: update articles

---
+++ packages/ueact-vdom/types.d.ts
@@ -0,0 +1,14 @@
+declare module '*.less' {
+  const styles: Record<string, string>;
+  export = styles;
+}
+
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
