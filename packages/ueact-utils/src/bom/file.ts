2e50f9447b659d14b0f1b889e1093561326fd85a
wx-chevalier <wxchevalier925@gmail.com>
2020-03-15 14:07:50 +0800

    feat: update articles or snippets

--- packages/ueact-utils/src/bom/file.ts
+++ packages/ueact-utils/src/bom/file.ts
@@ -18,10 +18,26 @@ export function downloadUrl(url: string, fileName: string = '临时五年级') {
 /** 将某个 Blob 变为文件对象 */
 export function blobToFile(theBlob: Blob, fileName: string = 'fileName'): File {
   var b: any = theBlob;
-  //A Blob() is almost a File() - it's just missing the two properties below which we will add
+  // A Blob() is almost a File() - it's just missing the two properties below which we will add
   b.lastModifiedDate = new Date();
   b.name = fileName;
 
-  //Cast to a File() type
+  // Cast to a File() type
   return <File>theBlob;
+}
+
+/** 将某个文件对象转化为 ArrayBuffer */
+export function readFileAsArrayBufferAsync(file: File): Promise<ArrayBuffer> {
+  return new Promise((resolve, reject) => {
+    const fr = new FileReader();
+    fr.onload = () => {
+      const data = fr.result as ArrayBuffer;
+      resolve(data);
+    };
+    fr.onerror = e => {
+      reject(e);
+    };
+
+    fr.readAsArrayBuffer(file);
+  });
 }
